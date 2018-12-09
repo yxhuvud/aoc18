bbde33c2e97747d353f6bb8ccdafa3a4e63ad24a
Linus Sellberg <sellberg@gmail.com>
2018-12-09 12:52:00 +0100

    Day9: It works perfectly fine to start with 1.

--- day9.cr
+++ day9.cr
@@ -1,8 +1,7 @@
 def play(players, stop)
-  marbles = 2..stop
   scores = [0_i64] * players
-  circle = Deque(Int32).new([1, 0])
-  marbles.each do |i|
+  circle = Deque(Int32).new([0])
+  (1..stop).each do |i|
     if i % 23 == 0
       circle.rotate!(-7)
       scores[i % players] += circle.shift + i
